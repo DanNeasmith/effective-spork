8de53ac2517312f9bc4cbdad21f296fcaaed16da
Tom-Sloan <tsloan81@gmail.com>
2021-05-12 13:25:25 -0400

    huge chsang

---
+++ mixxedMessages.js
@@ -0,0 +1,13 @@
+function makeid() {
+    let result           = [];
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    let length = Math.floor(Math.random()*1000);
+    for ( let i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
+}
+
+console.log(makeid())
